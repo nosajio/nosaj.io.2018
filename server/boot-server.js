6c24f5dbac7fb65d6fae6f302dc834947d74661b
Jason Howmans <jason@nosaj.io>
2018-06-03 22:35:02 +0100

    Serve static assets

--- server/boot-server.js
+++ server/boot-server.js
@@ -1,28 +1,34 @@
 // Boot the server by bundling assets and keeping them in memory until they're 
 // requested
 
-const Koa = require('koa');
-const app = new Koa();
+const Koa       = require('koa');
+const koaStatic = require('koa-static');
+const app       = new Koa();
 
 const { bundleToMemory, cssToMemory } = require('./compiler/compile-assets');
 const { log, error } = require('server/logging')('server');
-
-const listenPort = process.env.PORT || 3012;
+const { fromBase } = require('constants/paths');
 
 const renderError = require('./renderers/errors/500');
 const renderHomepage = require('./renderers/homepage-renderer');
 
-const bootServer = async ({ css, js }) => {
+const listenPort = process.env.PORT || 3012;
+
+const bootServer = async ({ css, js, staticPath }) => {
   // Bundle up scripts and stylesheets and save to memory on boot. Then we can
   // rapidly get to them when a request comes in.
   await bundleToMemory(js);
   await cssToMemory(css).catch(err => error(err));
 
+  // Setup the static middleware, which will serve static assets from a directory
+  log(fromBase(staticPath))
+  app.use( koaStatic(fromBase(staticPath)) )
 
   // Setup the one and only request handler
   app.use(async ctx => {
     const { request, response } = ctx;
-    // TODO if this gets to > a few routes, refactor into simple route => renderer function
+    // TODO use koa-router when it's time to make the blog...
+    // => https://github.com/alexmingoia/koa-router
     switch (request.url) {
       // Home route
       case '/':
@@ -34,6 +40,7 @@ const bootServer = async ({ css, js }) => {
           ctx.body = renderError();
         }
         break;
+      case '/r':
     }
     return;
   });
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const boot = require('./boot-server');
 
 const withAssets = {
+  staticPath: './web/static',
   js: ['./web/index.js'],
   css: ['./web/views/homepage/homepage.css']
 }
