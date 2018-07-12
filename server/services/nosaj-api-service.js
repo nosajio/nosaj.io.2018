78f6864804624e473172d23f491671e61f258423
Jason Howmans <jason@nosaj.io>
2018-07-13 00:31:17 +0100

    Use MemoryCache in posts api service.

--- server/services/nosaj-api-service.js
+++ server/services/nosaj-api-service.js
@@ -1,19 +1,29 @@
 const axios = require('axios');
 const { log, error } = require('../logging')('nosaj-api-service');
 const nosajApiUrl = require('constants/urls').nosajApi;
+const Cache = require('../utils/memory-cache');
+
+// Configure the cache for posts data
+const postsCache = new Cache({
+  expires: Cache.minsToMs(15)
+});
 
 const getEndpointUrl = (path) => `${nosajApiUrl}/${path}`;
 
-const getPost = async slug => { 
-  const postEndpoint = getEndpointUrl(`posts`);
-  const post = await axios.get(postEndpoint);
-  return post.data.find(p => p.slug === slug);
-}
-
 const getPosts = async () => {
   const postsEndpoint = getEndpointUrl('posts')
+  const fromCache = postsCache.get(postsEndpoint);
+  if (fromCache) {
+    return fromCache;
+  }
   const posts = await axios.get(postsEndpoint);
+  postsCache.set(postsEndpoint, posts.data);
   return posts.data;
+}
+
+const getPost = async slug => {
+  const posts = await getPosts();
+  return posts.data.find(p => p.slug === slug);
 }
 
 module.exports = { getPosts, getPost }
